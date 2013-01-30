7e33a00ad174c9c846eb7dacafdcd3fb8260474a
Tobias Adam <tow8ie@gmail.com>
2013-01-30 12:26:10 +0100

    Added Gemfile and fixed wrong configuration access in test helper.

--- test/irt_helper.rb
+++ test/irt_helper.rb
@@ -8,5 +8,5 @@ INDEX_NAME_1   = 'flex_test_index_1'
 INDEX_NAME_2   = 'flex_test_index_2'
 
 Flex.create_index(:index => INDEX_NAME) unless Flex.exist?(:index => INDEX_NAME)
-Flex.config.variables[:index] = INDEX_NAME
+Flex::Configuration.variables[:index] = INDEX_NAME
 Flex.reload!
